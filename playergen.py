f91c0c46b4c467829fac9e2b3abdc324d92a4812
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2020-12-22 10:11:43 -0500

    Create playergen.py

---
+++ playergen.py
@@ -0,0 +1,5 @@
+def playergen(position, type)
+  firstnamelist = ['super', 'diamond', 'cool', 'fire', 'water', 'ice', 'computer', 'party', 'peace', 'burger', 'party', "ryan's", 'new', 'cute', 'magma', 'plasma', 'nutty', 'cash', 'awesome', 'speedy', 'power', 'chocolate', 'purple', 'red', 'blue', 'snow', 'santa', 'pink', 'gold', 'silver', 'bronze', 'big', 'little', "alex's", 'poison', 'rose', 'zoom', 'nitro', 'lava', 'aqua', 'orange', 'strong', 'wind', 'earth', 'flame', 'psychic', 'wolf', 'steam']
+  lastnamelist = ['Smallie', 'Clippy', 'Tsum', 'Biggie', 'Mouse', 'Jumb', 'Bumb']
+  narwhalnamelist = ['Sleepy', 'Fuzzy', 'Mr', 'Norman', 'Pusheen', 'Pushane', 'Shane', 'Stress', 'Cute']
+  stats = []
