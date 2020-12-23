4b58797a00b64c2d0dff2d3f118055cecd8f49a1
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2020-12-23 09:17:22 -0500

    Update playergen.py
    can now generate a normal striker. Framework added for other types and positions

--- playergen.py
+++ playergen.py
@@ -1,33 +1,38 @@
 import random
 
-def statgen(position, type, id='0'):
+def statgen(position, kind, id='0'):
   pace = ''
-  shooting = ''
+  shot = ''
   passing = ''
   dribbling = ''
   defence = ''
   physical = ''
-  
-  stats = ['Pace: '+pace, 'Shooting: '+shooting, 'Passing: '+passing, 'Dribbling: '+dribbling, 'Defence: '+defence, 'Physical: '+physical]
-  if type = '1':
-    if position = 'st':
-        pace = str(random.randint(76, 91)
-        shooting = str(random.randint(79, 93)
-        passing = str(random.randint(74, 88)
-        dribbling = str(random.randint(76, 89)
-        defence = str(random.randint(30, 60)
-        physical = str(random.randint(75, 94)
-   return stats
 
-def playergen(position, type, id='0')
-  firstnamelist = ['super', 'diamond', 'cool', 'fire', 'water', 'ice', 'computer', 'party', 'peace', 'burger', 'party', "ryan's", 'new', 'cute', 'magma', 'plasma', 'nutty', 'cash', 'awesome', 'speedy', 'power', 'chocolate', 'purple', 'red', 'blue', 'snow', 'santa', 'pink', 'gold', 'silver', 'bronze', 'big', 'little', "alex's", 'poison', 'rose', 'zoom', 'nitro', 'lava', 'aqua', 'orange', 'strong', 'wind', 'earth', 'flame', 'psychic', 'wolf', 'steam']
-  lastnamelist = ['Smallie', 'Clippy', 'Tsum', 'Biggie', 'Mouse', 'Jumb', 'Bumb']
+  if kind == '1':
+    if position == 'st':
+        pace = str(random.randint(76, 91))
+        shot = str(random.randint(79, 93))   
+        passing = str(random.randint(74, 88))
+        dribbling = str(random.randint(76, 89))
+        defence = str(random.randint(30, 60))
+        physical = str(random.randint(75, 94))
+        stats = ['Pace: '+pace+'', 'Shooting: '+shot+'', 'Passing: '+passing+'', 'Dribbling: '+dribbling+'', 'Defence: '+defence+'', 'Physical: '+physical+'']
+  else:
+    print('u messed up')
+
+  return stats
+
+def playergen(position, kind, id='0'):
+  firstnamelist = ['Super', 'Diamond', 'Cool', 'Fire', 'Water', 'Ice', 'Computer', 'Party', 'Peace', 'Burger', 'Party', "Ryan's", 'New', 'Cute', 'Magma', 'Plasma', 'Nutty', 'Cash', 'Awesome', 'Speedy', 'Power', 'Chocolate', 'Purple', 'Red', 'Blue', 'Snow', 'Winter', 'Pink', 'Gold', 'Silver', 'Bronze', 'Big', 'Little', "Alex's", 'Poison', 'Rose', 'Zoom', 'Nitro', 'Lava', 'Aqua', 'Orange', 'Strong', 'Wind', 'Earth', 'Flame', 'Psychic', 'Wolf', 'Steam']
+  lastnamelist = ['Smallie', 'Clippy', 'Tsum', 'Biggie', 'Mouse', 'Jumb', 'Bumb', 'Cheb', 'Mouse']
   narwhalnamelist = ['Sleepy', 'Fuzzy', 'Mr', 'Norman', 'Pusheen', 'Pushane', 'Shane', 'Stress', 'Cute']
   stats = []
   #types: 1 = normal, 2 = icon, 3 = scream, 4 = totw1, 5 = totw2
-  if type = '1':
-    if position = 'st':
-      stats = statgen('st', '1')
-      print statgen                 
+  if kind == '1':
+    if position == 'st':
+      stats = statgen('st', '1', '0')
+      stats.append(random.choice(firstnamelist)+' '+random.choice(lastnamelist))
+      print(stats)                 
                        
       
+playergen('st', '1')
